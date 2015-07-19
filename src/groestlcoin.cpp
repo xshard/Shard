8691ca569be87f5990e8c6f8eafc7513ade5df78
ufasoft <dev@ufasoft.com>
2015-07-19 12:43:42 +0500

    -

---
+++ src/groestlcoin.cpp
@@ -0,0 +1,8 @@
+#include "groestlcoin.h"
+
+
+#ifdef _WIN32
+#	pragma comment(lib, "ws2_32")
+
+#endif _WIN32
+
--- src/grs-config.h
+++ src/grs-config.h
@@ -1,10 +1,32 @@
 #pragma once
 
-#pragma warning(disable: 4018 4244 4267 4290 4800)
+#define UCFG_DEFINE_NDEBUG 0
+
+#include <el/libext.h>
+
+
+#pragma warning(disable: 4018 4242 4244 4267 4290 4800)
 
 #define WIN32
 
 #define _CRT_SECURE_NO_DEPRECATE 1
 #define _SCL_SECURE_NO_WARNINGS
+
+#define HAVE_DECL_HTOBE16 1
+#define HAVE_DECL_HTOLE16 1
+#define HAVE_DECL_BE16TOH 1
+#define HAVE_DECL_LE16TOH 1
+#define HAVE_DECL_HTOLE32 1
+#define HAVE_DECL_BE32TOH 1
+#define HAVE_DECL_LE32TOH 1
+#define HAVE_DECL_HTOBE64 1
+#define HAVE_DECL_HTOLE64 1
+#define HAVE_DECL_BE64TOH 1
+#define HAVE_DECL_LE64TOH 1
+
+
+
+
+
 
 
