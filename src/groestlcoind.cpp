3642356debfb978db4065fc23dc627339779e3b6
ufasoft <dev@ufasoft.com>
2015-07-28 16:35:45 +0500

    -

--- src/groestlcoind.cpp
+++ src/groestlcoind.cpp
@@ -115,7 +115,7 @@ bool AppInit(int argc, char* argv[])
 
         if (fCommandLine)
         {
-            fprintf(stderr, "Error: There is no RPC client functionality in groestlcoind anymore. Use the bitcoin-cli utility instead.\n");
+            fprintf(stderr, "Error: There is no RPC client functionality in groestlcoind anymore. Use the groestlcoin-cli utility instead.\n");
             exit(1);
         }
 #ifndef WIN32
