cef423c4e30f66ad0c6907364e012fce9b1b3eeb
ufasoft <dev@ufasoft.com>
2015-08-10 15:58:03 +0500

    -

--- contrib/spendfrom/spendfrom.py
+++ contrib/spendfrom/spendfrom.py
@@ -35,10 +35,10 @@ def check_json_precision():
 def determine_db_dir():
     """Return the default location of the bitcoin data directory"""
     if platform.system() == "Darwin":
-        return os.path.expanduser("~/Library/Application Support/Bitcoin/")
+        return os.path.expanduser("~/Library/Application Support/GroestlCoin/")
     elif platform.system() == "Windows":
-        return os.path.join(os.environ['APPDATA'], "Bitcoin")
-    return os.path.expanduser("~/.bitcoin")
+        return os.path.join(os.environ['APPDATA'], "GroestlCoin")
+    return os.path.expanduser("~/.groestlcoin")
 
 def read_bitcoin_config(dbdir):
     """Read the bitcoin.conf file from dbdir, returns dictionary of settings"""
@@ -59,7 +59,7 @@ def read_bitcoin_config(dbdir):
                 return s
 
     config_parser = SafeConfigParser()
-    config_parser.readfp(FakeSecHead(open(os.path.join(dbdir, "bitcoin.conf"))))
+    config_parser.readfp(FakeSecHead(open(os.path.join(dbdir, "groestlcoin.conf"))))
     return dict(config_parser.items("all"))
 
 def connect_JSON(config):
