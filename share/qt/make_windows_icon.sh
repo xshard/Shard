35c12fbc2afbd9d1eae811938aa74d30a93b4a0f
ufasoft <dev@ufasoft.com>
2015-08-10 20:07:53 +0500

    -

--- share/qt/make_windows_icon.sh
+++ share/qt/make_windows_icon.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # create multiresolution windows icon
-ICON_SRC=../../src/qt/res/icons/bitcoin.png
-ICON_DST=../../src/qt/res/icons/bitcoin.ico
-convert ${ICON_SRC} -resize 16x16 bitcoin-16.png
-convert ${ICON_SRC} -resize 32x32 bitcoin-32.png
-convert ${ICON_SRC} -resize 48x48 bitcoin-48.png
-convert bitcoin-16.png bitcoin-32.png bitcoin-48.png ${ICON_DST}
+ICON_SRC=../../src/qt/res/icons/groestlcoin.png
+ICON_DST=../../src/qt/res/icons/groestlcoin.ico
+convert ${ICON_SRC} -resize 16x16 groestlcoin-16.png
+convert ${ICON_SRC} -resize 32x32 groestlcoin-32.png
+convert ${ICON_SRC} -resize 48x48 groestlcoin-48.png
+convert groestlcoin-16.png groestlcoin-32.png groestlcoin-48.png ${ICON_DST}
 
