5b99a0d5addea36e87e8c95a5ec6363f4e80bcce
ufasoft <dev@ufasoft.com>
2016-12-17 19:46:51 +0500

    Define dates of consensus.vDeployments

--- src/groestlcoin.cpp
+++ src/groestlcoin.cpp
@@ -331,15 +331,26 @@ public:
 		consensus.BIP66Height = 800000;
 		consensus.BIP65Height = INT_MAX;	//!!!?
 
-//!!!?        consensus.nMajorityEnforceBlockUpgrade = 750;
-//!!!?        consensus.nMajorityRejectBlockOutdated = 950;
-//!!!?        consensus.nMajorityWindow = 1000;
         consensus.powLimit = uint256S("00000fffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
         consensus.nPowTargetTimespan = 14 * 24 * 60 * 60; // two weeks
         consensus.nPowTargetSpacing = 60;
         consensus.fPowAllowMinDifficultyBlocks = false;
         consensus.nRuleChangeActivationThreshold = 1916; // 95% of 2016
 		consensus.nMinerConfirmationWindow = 2016;
+		consensus.vDeployments[Consensus::DEPLOYMENT_TESTDUMMY].bit = 28;
+		consensus.vDeployments[Consensus::DEPLOYMENT_TESTDUMMY].nStartTime = 1199145601; // January 1, 2008
+		consensus.vDeployments[Consensus::DEPLOYMENT_TESTDUMMY].nTimeout = 1230767999; // December 31, 2008
+
+		// Deployment of BIP68, BIP112, and BIP113.
+		consensus.vDeployments[Consensus::DEPLOYMENT_CSV].bit = 0;
+		consensus.vDeployments[Consensus::DEPLOYMENT_CSV].nStartTime = 1493596800; // May 1st, 2017
+		consensus.vDeployments[Consensus::DEPLOYMENT_CSV].nTimeout = 1525132800; // May 1st, 2018
+
+		// Deployment of SegWit (BIP141 and BIP143)
+		consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].bit = 1;
+		consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nStartTime = 1496275200; // June 1st, 2017
+		consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nTimeout = 1527811200; // June 1st, 2018
+
 		/**
          * The message start string is designed to be unlikely to occur in normal data.
          * The characters are rarely used upper ASCII, not valid as UTF-8, and produce
@@ -439,10 +450,20 @@ public:
 		consensus.fPowAllowMinDifficultyBlocks = true;
         consensus.nRuleChangeActivationThreshold = 1512; // 75% for testchains
 		consensus.nMinerConfirmationWindow = 2016;
+		consensus.vDeployments[Consensus::DEPLOYMENT_TESTDUMMY].bit = 28;
+		consensus.vDeployments[Consensus::DEPLOYMENT_TESTDUMMY].nStartTime = 1199145601; // January 1, 2008
+		consensus.vDeployments[Consensus::DEPLOYMENT_TESTDUMMY].nTimeout = 1230767999; // December 31, 2008
 
-//!!!T        consensus.nMajorityEnforceBlockUpgrade = 51;
-//!!!T        consensus.nMajorityRejectBlockOutdated = 75;
-//!!!T        consensus.nMajorityWindow = 100;
+		// Deployment of BIP68, BIP112, and BIP113.
+		consensus.vDeployments[Consensus::DEPLOYMENT_CSV].bit = 0;
+		consensus.vDeployments[Consensus::DEPLOYMENT_CSV].nStartTime = 1485907200; // Feb 1st, 2017
+		consensus.vDeployments[Consensus::DEPLOYMENT_CSV].nTimeout = 1517443200; // Feb 1st, 2018
+
+		// Deployment of SegWit (BIP141, BIP143, and BIP147)
+		consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].bit = 1;
+		consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nStartTime = 1488326400; // Mar 1st, 2017
+		consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nTimeout = 1519862400; // Mar 1st, 2018
+
 		consensus.powLimit = uint256S("000000ffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
         pchMessageStart[0] = 0x0b;
         pchMessageStart[1] = 0x11;
@@ -512,11 +533,17 @@ public:
 		consensus.fPowAllowMinDifficultyBlocks = true;
         consensus.nRuleChangeActivationThreshold = 108; // 75% for testchains
 		consensus.nMinerConfirmationWindow = 144; // Faster than normal for regtest (144 instead of 2016)
+		consensus.vDeployments[Consensus::DEPLOYMENT_TESTDUMMY].bit = 28;
+		consensus.vDeployments[Consensus::DEPLOYMENT_TESTDUMMY].nStartTime = 0;
+		consensus.vDeployments[Consensus::DEPLOYMENT_TESTDUMMY].nTimeout = 999999999999ULL;
+		consensus.vDeployments[Consensus::DEPLOYMENT_CSV].bit = 0;
+		consensus.vDeployments[Consensus::DEPLOYMENT_CSV].nStartTime = 0;
+		consensus.vDeployments[Consensus::DEPLOYMENT_CSV].nTimeout = 999999999999ULL;
+		consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].bit = 1;
+		consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nStartTime = 0;
+		consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nTimeout = 999999999999ULL;
 
-//!!!T        consensus.nMajorityEnforceBlockUpgrade = 750;
-//!!!T        consensus.nMajorityRejectBlockOutdated = 950;
-//!!!T        consensus.nMajorityWindow = 1000;
-        pchMessageStart[0] = 0xfa;
+		pchMessageStart[0] = 0xfa;
         pchMessageStart[1] = 0xbf;
         pchMessageStart[2] = 0xb5;
         pchMessageStart[3] = 0xda;
