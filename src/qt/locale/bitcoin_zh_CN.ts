4f5b15ff06ff9d6db5749b2d26fba96c3dba1d31
ufasoft <dev@ufasoft.com>
2017-06-25 04:01:15 +0500

    Change Chinese locale string

--- src/qt/locale/bitcoin_zh_CN.ts
+++ src/qt/locale/bitcoin_zh_CN.ts
@@ -1,4 +1,4 @@
-<TS language="zh_CN" version="2.1">
+﻿<TS language="zh_CN" version="2.1">
 <context>
     <name>AddressBookPage</name>
     <message>
@@ -63,11 +63,11 @@
     </message>
     <message>
         <source>These are your Groestlcoin addresses for sending payments. Always check the amount and the receiving address before sending coins.</source>
-        <translation>这些是你要付款过去的复述币地址。在付钱之前，务必要检查金额和收款地址是否正确。</translation>
+        <translation>这些是你要付款过去的闪电币地址。在付钱之前，务必要检查金额和收款地址是否正确。</translation>
     </message>
     <message>
         <source>These are your Groestlcoin addresses for receiving payments. It is recommended to use a new receiving address for each transaction.</source>
-        <translation>这些是你用来收款的复述币地址。建议在每次交易时，都使用一个新的收款地址。</translation>
+        <translation>这些是你用来收款的闪电币地址。建议在每次交易时，都使用一个新的收款地址。</translation>
     </message>
     <message>
         <source>&amp;Copy Address</source>
@@ -169,7 +169,7 @@
     </message>
     <message>
         <source>Warning: If you encrypt your wallet and lose your passphrase, you will &lt;b&gt;LOSE ALL OF YOUR GROESTLCOINS&lt;/b&gt;!</source>
-        <translation>警告: 如果把钱包加密后又忘记密码，你就会从此&lt;b&gt;失去其中所有的复述币了&lt;/b&gt;！</translation>
+        <translation>警告: 如果把钱包加密后又忘记密码，你就会从此&lt;b&gt;失去其中所有的闪电币了&lt;/b&gt;！</translation>
     </message>
     <message>
         <source>Are you sure you wish to encrypt your wallet?</source>
@@ -323,7 +323,7 @@
     </message>
     <message>
         <source>Send coins to a Groestlcoin address</source>
-        <translation>向一个复述币地址发送复述币</translation>
+        <translation>向一个闪电币地址发送闪电币</translation>
     </message>
     <message>
         <source>Backup wallet to another location</source>
@@ -347,7 +347,7 @@
     </message>
     <message>
         <source>Groestlcoin</source>
-        <translation>复述币</translation>
+        <translation>闪电币</translation>
     </message>
     <message>
         <source>Wallet</source>
@@ -375,11 +375,11 @@
     </message>
     <message>
         <source>Sign messages with your Groestlcoin addresses to prove you own them</source>
-        <translation>用复述币地址关联的私钥为消息签名，以证明您拥有这个复述币地址</translation>
+        <translation>用闪电币地址关联的私钥为消息签名，以证明您拥有这个闪电币地址</translation>
     </message>
     <message>
         <source>Verify messages to ensure they were signed with specified Groestlcoin addresses</source>
-        <translation>校验消息，确保该消息是由指定的复述币地址所有者签名的</translation>
+        <translation>校验消息，确保该消息是由指定的闪电币地址所有者签名的</translation>
     </message>
     <message>
         <source>&amp;File</source>
@@ -419,7 +419,7 @@
     </message>
     <message numerus="yes">
         <source>%n active connection(s) to Groestlcoin network</source>
-        <translation><numerusform>%n 个到复述币网络的活动连接</numerusform></translation>
+        <translation><numerusform>%n 个到闪电币网络的活动连接</numerusform></translation>
     </message>
     <message>
         <source>Indexing blocks on disk...</source>
@@ -803,7 +803,7 @@
     </message>
     <message>
         <source>The entered address "%1" is not a valid Groestlcoin address.</source>
-        <translation>输入的地址 %1 并不是有效的复述币地址。</translation>
+        <translation>输入的地址 %1 并不是有效的闪电币地址。</translation>
     </message>
     <message>
         <source>The entered address "%1" is already in the address book.</source>
@@ -913,7 +913,7 @@
     </message>
     <message>
         <source>%1 will download and store a copy of the Groestlcoin block chain. At least %2GB of data will be stored in this directory, and it will grow over time. The wallet will also be stored in this directory.</source>
-        <translation>%1 会下载并存储一份复述币区块链的副本。至少有 %2GB 的数据会存储到这个目录中，并且还会持续增长。另外钱包资料也会储存在这个目录。</translation>
+        <translation>%1 会下载并存储一份闪电币区块链的副本。至少有 %2GB 的数据会存储到这个目录中，并且还会持续增长。另外钱包资料也会储存在这个目录。</translation>
     </message>
     <message>
         <source>Use the default data directory</source>
@@ -1059,7 +1059,7 @@
     </message>
     <message>
         <source>Automatically open the Groestlcoin client port on the router. This only works when your router supports UPnP and it is enabled.</source>
-        <translation>自动在路由器中打开复述币端口。只有当您的路由器开启了 UPnP 选项时此功能才有效。</translation>
+        <translation>自动在路由器中打开闪电币端口。只有当您的路由器开启了 UPnP 选项时此功能才有效。</translation>
     </message>
     <message>
         <source>Map port using &amp;UPnP</source>
@@ -1067,7 +1067,7 @@
     </message>
     <message>
         <source>Connect to the Groestlcoin network through a SOCKS5 proxy.</source>
-        <translation>通过 SOCKS5 代理连接复述币网络。</translation>
+        <translation>通过 SOCKS5 代理连接闪电币网络。</translation>
     </message>
     <message>
         <source>&amp;Connect through SOCKS5 proxy (default proxy):</source>
@@ -1107,7 +1107,7 @@
     </message>
     <message>
         <source>Connect to the Groestlcoin network through a separate SOCKS5 proxy for Tor hidden services.</source>
-        <translation>在 Tor 匿名网络下通过不同的 SOCKS5 代理连接复述币网络</translation>
+        <translation>在 Tor 匿名网络下通过不同的 SOCKS5 代理连接闪电币网络</translation>
     </message>
     <message>
         <source>Use separate SOCKS5 proxy to reach peers via Tor hidden services:</source>
@@ -1151,11 +1151,11 @@
     </message>
     <message>
         <source>&amp;Unit to show amounts in:</source>
-        <translation>复述币金额单位(&amp;U)：</translation>
+        <translation>闪电币金额单位(&amp;U)：</translation>
     </message>
     <message>
         <source>Choose the default subdivision unit to show in the interface and when sending coins.</source>
-        <translation>选择复述币单位。</translation>
+        <translation>选择闪电币单位。</translation>
     </message>
     <message>
         <source>Whether to show coin control features or not.</source>
@@ -1206,7 +1206,7 @@
     </message>
     <message>
         <source>The displayed information may be out of date. Your wallet automatically synchronizes with the Groestlcoin network after a connection is established, but this process has not completed yet.</source>
-        <translation>现在显示的消息可能是过期的。在连接上复述币网络节点后，您的钱包将自动与网络同步，但是这个过程还没有完成。</translation>
+        <translation>现在显示的消息可能是过期的。在连接上闪电币网络节点后，您的钱包将自动与网络同步，但是这个过程还没有完成。</translation>
     </message>
     <message>
         <source>Watch-only:</source>
@@ -1298,7 +1298,7 @@
     </message>
     <message>
         <source>URI cannot be parsed! This can be caused by an invalid Groestlcoin address or malformed URI parameters.</source>
-        <translation>无法解析 URI 地址！可能是因为复述币地址无效，或是 URI 参数格式错误。</translation>
+        <translation>无法解析 URI 地址！可能是因为闪电币地址无效，或是 URI 参数格式错误。</translation>
     </message>
     <message>
         <source>Payment request file handling</source>
@@ -1348,7 +1348,7 @@
     </message>
     <message>
         <source>Enter a Groestlcoin address (e.g. %1)</source>
-        <translation>请输入一个复述币地址 (例如 %1)</translation>
+        <translation>请输入一个闪电币地址 (例如 %1)</translation>
     </message>
     <message>
         <source>%1 d</source>
@@ -1709,7 +1709,7 @@
     </message>
     <message>
         <source>An optional message to attach to the payment request, which will be displayed when the request is opened. Note: The message will not be sent with the payment over the Groestlcoin network.</source>
-        <translation>可在付款请求上备注一条信息，在打开付款请求时可以看到。注意：该消息不是通过复述币网络传送。</translation>
+        <translation>可在付款请求上备注一条信息，在打开付款请求时可以看到。注意：该消息不是通过闪电币网络传送。</translation>
     </message>
     <message>
         <source>An optional label to associate with the new receiving address.</source>
@@ -1838,7 +1838,7 @@
     <name>SendCoinsDialog</name>
     <message>
         <source>Send Coins</source>
-        <translation>发送复述币</translation>
+        <translation>发送闪电币</translation>
     </message>
     <message>
         <source>Coin Control Features</source>
@@ -2030,7 +2030,7 @@
     </message>
     <message>
         <source>Warning: Invalid Groestlcoin address</source>
-        <translation>警告: 复述币地址无效</translation>
+        <translation>警告: 闪电币地址无效</translation>
     </message>
     <message>
         <source>(no label)</source>
@@ -2081,7 +2081,7 @@
     </message>
     <message>
         <source>The fee will be deducted from the amount being sent. The recipient will receive less groestlcoins than you enter in the amount field. If multiple recipients are selected, the fee is split equally.</source>
-        <translation>交易费将从发送总额中扣除。接收人将收到比您在金额框中输入的更少的复述币。如果选中了多个收件人，交易费平分。</translation>
+        <translation>交易费将从发送总额中扣除。接收人将收到比您在金额框中输入的更少的闪电币。如果选中了多个收件人，交易费平分。</translation>
     </message>
     <message>
         <source>S&amp;ubtract fee from amount</source>
@@ -2105,7 +2105,7 @@
     </message>
     <message>
         <source>A message that was attached to the groestlcoin: URI which will be stored with the transaction for your reference. Note: This message will not be sent over the Groestlcoin network.</source>
-        <translation>groestlcoin:URI 附带的备注信息，将会和交易一起存储，备查。 注意：该消息不会通过复述币网络传输。</translation>
+        <translation>groestlcoin:URI 附带的备注信息，将会和交易一起存储，备查。 注意：该消息不会通过闪电币网络传输。</translation>
     </message>
     <message>
         <source>Pay To:</source>
@@ -2146,7 +2146,7 @@
     </message>
     <message>
         <source>You can sign messages/agreements with your addresses to prove you can receive groestlcoins sent to them. Be careful not to sign anything vague or random, as phishing attacks may try to trick you into signing your identity over to them. Only sign fully-detailed statements you agree to.</source>
-        <translation>您可以用你的地址对消息/协议进行签名，以证明您可以接收发送到该地址的复述币。注意不要对任何模棱两可或者随机的消息进行签名，以免遭受钓鱼式攻击。请确保消息内容准确的表达了您的真实意愿。</translation>
+        <translation>您可以用你的地址对消息/协议进行签名，以证明您可以接收发送到该地址的闪电币。注意不要对任何模棱两可或者随机的消息进行签名，以免遭受钓鱼式攻击。请确保消息内容准确的表达了您的真实意愿。</translation>
     </message>
     <message>
         <source>The Groestlcoin address to sign the message with</source>
@@ -2210,7 +2210,7 @@
     </message>
     <message>
         <source>Verify the message to ensure it was signed with the specified Groestlcoin address</source>
-        <translation>验证消息，确保消息是由指定的复述币地址签名过的。</translation>
+        <translation>验证消息，确保消息是由指定的闪电币地址签名过的。</translation>
     </message>
     <message>
         <source>Verify &amp;Message</source>
