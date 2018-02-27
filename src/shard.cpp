8b65c9da1e408ff267d9c8f1059ce12b462b3417
Joe Nilan <joenilan@gmail.com>
2018-02-27 04:14:28 -0500

    current epoch time

--- src/shard.cpp
+++ src/shard.cpp
@@ -371,7 +371,7 @@ public:
         nPruneAfterHeight = 10000000;
 
 
-		genesis = CreateGenesisBlock(1395342829, 220035, 0x1e0fffff, 112, 0);
+		genesis = CreateGenesisBlock(1519722675, 220035, 0x1e0fffff, 112, 0);
 
         /**
          * Build the genesis block. Note that the output of its generation
@@ -424,7 +424,7 @@ public:
         checkpointData = (CCheckpointData){
 #endif
             boost::assign::map_list_of
-            (0, uint256S("0x90aac702d3c49da1683ad70dd6179171b8140958e29320a6fc05cb3e0e6b59f9")),
+            (1519722675, uint256S("0x90aac702d3c49da1683ad70dd6179171b8140958e29320a6fc05cb3e0e6b59f9")),
             0,
             0,
             0
@@ -482,7 +482,7 @@ public:
 		*/
 
         //! Modify the testnet genesis block so the timestamp is valid for a later start.
-		genesis = CreateGenesisBlock(1440000002, 6556309, 0x1e00ffff, 3, 0);
+		genesis = CreateGenesisBlock(1519722675, 6556309, 0x1e00ffff, 3, 0);
         consensus.hashGenesisBlock = genesis.GetHash();
             /*printf("%s\n", consensus.hashGenesisBlock.ToString().c_str());*/
         assert(consensus.hashGenesisBlock == uint256S("0x454eade1c21d1f5bb15184549fae6e4e8b2ea87bdad6dd0703cb6343e588059f"));
@@ -512,7 +512,7 @@ public:
 		checkpointData = (CCheckpointData){
 #endif
 			boost::assign::map_list_of
-			(0, uint256S("0x454eade1c21d1f5bb15184549fae6e4e8b2ea87bdad6dd0703cb6343e588059f")),
+			(1519722675, uint256S("0x454eade1c21d1f5bb15184549fae6e4e8b2ea87bdad6dd0703cb6343e588059f")),
 			0,
 			0,
 			0
